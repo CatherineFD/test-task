4cbcb10ccc22b346aa90125fe46645795a324e5c
CatherineFD <firsov4.catherine@yandex.ru>
2024-08-16 12:21:05 +0300

    the finished test task

---
+++ test-vue/src/components/UI/Button.vue
@@ -0,0 +1,29 @@
+<template>
+	<button>
+		{{ title }}
+	</button>
+
+</template>
+
+<script>
+
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style>
+button {
+			padding: 5px 10px;
+			border: 1px solid #016285;
+			border-radius: 8px;
+			background-color: #0162853c;
+			align-self: center;
+			color: #003f56;
+		}
+</style>
